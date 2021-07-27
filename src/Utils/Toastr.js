5e7f9ee02d63150462505c88855b3b4a9861239c
wovenadmin <hemafixysoft@gmail.com>
2021-07-27 07:50:03 +0100

    Fix toastr

--- src/Utils/Toastr.js
+++ src/Utils/Toastr.js
@@ -1,7 +1,18 @@
 import { throttle } from "lodash";
+import { ToastContainer, toast } from "react-toastify";
+
 var numberOfCalls = 1;
 var message;
 var messageType;
+const toastrOptions = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
 export default function (_messageType, _message) {
   message = _message;
   messageType = _messageType;
@@ -11,7 +22,7 @@ export default function (_messageType, _message) {
 var throttledFunction = throttle(() => showMessage(), 1000);
 
 function showMessage() {
-  //console.log(numberOfCalls);
+  console.log(numberOfCalls);
   numberOfCalls++;
 
   var backgroundColor = "#83B739"; //success
@@ -22,13 +33,10 @@ function showMessage() {
     backgroundColor = "#ffc542";
   } else if (messageType == "error") {
     backgroundColor = "#fc5a5a";
+  } else if (messageType == "success") {
+    backgroundColor = "#83B739"
   }
-  eval(`$.toast({
-    heading: '',
-    text: '${message}',
-    showHideTransition: 'slide',
-    icon: '${messageType}',
-    loaderBg: '${backgroundColor}',
-    position: 'top-right'
-  })`);
+
+
+  toast[messageType](message, toastrOptions);
 }
